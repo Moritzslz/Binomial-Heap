366ec0a7f451f91a5607cccbb046ac87403c93da
moritzslz <schultzmoritz@gmail.com>
2023-06-28 01:32:39 +0200

    insert

--- src/gad/binomilia/BinomialHeap.java
+++ src/gad/binomilia/BinomialHeap.java
@@ -32,7 +32,7 @@ public class BinomialHeap {
 		}
 		ArrayList<BinomialTreeNode> nRoots = new ArrayList<>();
 		nRoots.add(nNode);
-		merge(nRoots);
+		roots = merge(roots, nRoots, 1);
 		result.addToIntermediateStep(roots);
 	}
 
@@ -45,7 +45,6 @@ public class BinomialHeap {
 		}
 	}
 
-	/*
 	public ArrayList<BinomialTreeNode> merge(ArrayList<BinomialTreeNode> a, ArrayList<BinomialTreeNode> b, int nB) {
 		ArrayList<BinomialTreeNode> nRoots = new ArrayList<>();
 		// Get the number of elements of each tree
@@ -56,36 +55,16 @@ public class BinomialHeap {
 			if (getBit(nA, i) == 1 && getBit(nB, i) == 1) {
 				// Merge trees of rank i
 				// Get root with rank i from a
-				BinomialTreeNode aRoot = a.get(a.size() - 1);
-				for (int aIdx = a.size() - 1; aIdx >= 0; aIdx--) {
-					if (a.get(aIdx).rank() == i) {
-						aRoot = a.get(aIdx);
-					}
-				}
+				BinomialTreeNode aRoot = a.get(i);
 				// Get root with rank i from b
-				BinomialTreeNode bRoot = b.get(b.size() - 1);
-				for (int bIdx = b.size() - 1; bIdx >= 0; bIdx--) {
-					if (b.get(bIdx).rank() == i) {
-						bRoot = a.get(bIdx);
-					}
-				}
+				BinomialTreeNode bRoot = b.get(i);
 				BinomialTreeNode nNode = BinomialTreeNode.merge(aRoot, bRoot);
 				nRoots.add(nNode);
 			} else if (getBit(nA, i) == 1) {
-				BinomialTreeNode aRoot = a.get(a.size() - 1);
-				for (int aIdx = a.size() - 1; aIdx >= 0; aIdx--) {
-					if (a.get(aIdx).rank() == i) {
-						aRoot = a.get(aIdx);
-					}
-				}
+				BinomialTreeNode aRoot = a.get(i);
 				nRoots.add(aRoot);
 			} else if (getBit(nB, i) == 1) {
-				BinomialTreeNode bRoot = b.get(0);
-				for (int bIdx = b.size() - 1; bIdx >= 0; bIdx--) {
-					if (b.get(bIdx).rank() == i) {
-						bRoot = a.get(bIdx);
-					}
-				}
+				BinomialTreeNode bRoot = b.get(i);
 				nRoots.add(bRoot);
 			} else {
 				// Add nothing
@@ -100,63 +79,6 @@ public class BinomialHeap {
 			}
 		}
 		return nRoots;
-	}
-	 */
-
-	private void merge(ArrayList<BinomialTreeNode> newTrees) {
-		int maxSize = Math.max(roots.size(), newTrees.size());
-		ArrayList<BinomialTreeNode> mergedTrees = new ArrayList<>(maxSize + 1);
-
-		int carry = 0;
-		for (int i = 0; i < maxSize; i++) {
-			BinomialTreeNode tree1 = i < roots.size() ? roots.get(i) : null;
-			BinomialTreeNode tree2 = i < newTrees.size() ? newTrees.get(i) : null;
-
-			int rank = carry;
-			if (tree1 != null) {
-				rank += tree1.rank();
-			}
-			if (tree2 != null) {
-				rank += tree2.rank();
-			}
-
-			if (tree1 == null && tree2 == null) {
-				break;
-			}
-
-			if (tree1 == null) {
-				carry = 0;
-			} else if (tree2 == null) {
-				carry = 0;
-			} else if (tree1.rank() == tree2.rank()) {
-				mergedTrees.add(null);
-				carry = 1;
-			} else {
-				carry = 0;
-			}
-		}
-
-		if (carry == 1) {
-			mergedTrees.add(null);
-		}
-
-		for (int i = 0; i < mergedTrees.size(); i++) {
-			BinomialTreeNode tree1 = i < roots.size() ? roots.get(i) : null;
-			BinomialTreeNode tree2 = i < newTrees.size() ? newTrees.get(i) : null;
-
-			BinomialTreeNode mergedTree;
-			if (tree1 == null) {
-				mergedTree = tree2;
-			} else if (tree2 == null) {
-				mergedTree = tree1;
-			} else {
-				mergedTree = BinomialTreeNode.merge(tree1, tree2);
-			}
-
-			mergedTrees.set(i, mergedTree);
-		}
-
-		roots = mergedTrees;
 	}
 
 	public static int getBit(int element, int binPlace) {
